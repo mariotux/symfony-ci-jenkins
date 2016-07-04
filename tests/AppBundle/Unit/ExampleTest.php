48137b693302520cdc8649b37402702d9bddb78c
Mario Nunes <mario@laptop.local>
2016-07-04 14:00:37 +0200

    first test

---
+++ tests/AppBundle/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tests\AppBundle\Unit;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
